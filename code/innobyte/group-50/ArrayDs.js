9c09ac1dafc95f587817197179a33143f5d53d16
Isaac Matthew <162550661+isaacmatthew1@users.noreply.github.com>
2024-03-31 18:34:18 +0100

    Updated ArrayDs.js - added class and contructor for the class
    Signed-off-by: Isaac Matthew <162550661+isaacmatthew1@users.noreply.github.com>

--- code/innobyte/group-50/ArrayDs.js
+++ code/innobyte/group-50/ArrayDs.js
@@ -1,1 +1,14 @@
 
+/**
+ * Simple array-based data structure.
+ * @class
+ * @classdesc Represents a basic data structure utilizing an array.
+ * @author isaac matthew
+ * @regno 20/csc/074
+ */
+class ArrayDataStructure {
+    constructor() {
+        // Initialize an empty array to store elements
+        this.data = [];
+    }
+}
