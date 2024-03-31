b06442ea221b942a0721854ef31ad9c07ee0e04a
Isaac Matthew <162550661+isaacmatthew1@users.noreply.github.com>
2024-03-31 18:42:51 +0100

    completed array data structure implementation in JS assigned to me
    Signed-off-by: Isaac Matthew <162550661+isaacmatthew1@users.noreply.github.com>

--- code/innobyte/group-50/ArrayDs.js
+++ code/innobyte/group-50/ArrayDs.js
@@ -11,4 +11,44 @@ class ArrayDataStructure {
         // Initialize an empty array to store elements
         this.data = [];
     }
+
+    /**
+     * Appends a new element to the array.
+     * @param {*} item - The item to be appended.
+     */
+    append(item) {
+        this.data.push(item);
+    }
+
+    /**
+     * Retrieves the element at the specified index.
+     * @param {number} index - The index of the element to retrieve.
+     * @returns {*} The element at the specified index, or undefined if the index is out of bounds.
+     */
+    access(index) {
+        if (index < 0 || index >= this.data.length) {
+            return undefined;
+        }
+        return this.data[index];
+    }
+
+    /**
+     * Removes the element at the specified index.
+     * @param {number} index - The index of the element to remove.
+     * @returns {*} The removed element, or undefined if the index is out of bounds.
+     */
+    erase(index) {
+        if (index < 0 || index >= this.data.length) {
+            return undefined;
+        }
+        return this.data.splice(index, 1)[0];
+    }
+
+    /**
+     * Returns the length of the array.
+     * @returns {number} The length of the array.
+     */
+    size() {
+        return this.data.length;
+    }
 }
