920965f077d86fb294afcc72cb6b93a5e099a94e
Clinboyedet <163317933+Clinboyedet@users.noreply.github.com>
2024-04-01 02:19:10 +0100

    Update hash-tables.py

--- code/jetstream/Jetstream Technologies Group 56/Project 2/Report-on-Project-2b/hash-tables.py
+++ code/jetstream/Jetstream Technologies Group 56/Project 2/Report-on-Project-2b/hash-tables.py
@@ -1,1 +1,18 @@
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(size)]
 
+    def hash_function(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        self.table[index].append((key, value))
+
+    def search(self, key):
+        index = self.hash_function(key)
+        for k, v in self.table[index]:
+            if k == key:
+                return v
+        return None
