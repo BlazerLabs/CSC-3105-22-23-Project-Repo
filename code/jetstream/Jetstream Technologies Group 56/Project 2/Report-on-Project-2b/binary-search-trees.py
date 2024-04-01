ba0dc7945ea8f7f1392d3075c6fa6b49f6cd4397
Clinboyedet <163317933+Clinboyedet@users.noreply.github.com>
2024-04-01 02:08:11 +0100

    Update binary-search-trees.py

--- code/jetstream/Jetstream Technologies Group 56/Project 2/Report-on-Project-2b/binary-search-trees.py
+++ code/jetstream/Jetstream Technologies Group 56/Project 2/Report-on-Project-2b/binary-search-trees.py
@@ -1,1 +1,31 @@
+class BSTNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
 
+    def insert(self, key):
+        if key < self.key:
+            if self.left is None:
+                self.left = BSTNode(key)
+            else:
+                self.left.insert(key)
+        elif key > self.key:
+            if self.right is None:
+                self.right = BSTNode(key)
+            else:
+                self.right.insert(key)
+
+    def search(self, key):
+        if self.key == key:
+            return True
+        elif key < self.key:
+            if self.left is None:
+                return False
+            else:
+                return self.left.search(key)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.search(key)
