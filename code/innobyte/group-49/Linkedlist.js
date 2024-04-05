af8b27daff4a21a5e7a21bf6553faf790d4cab39
WhilzWurld <162498205+WhilzWurld@users.noreply.github.com>
2024-04-05 16:10:31 +0100

    Update Linkedlist.js
    Signed-off-by: WhilzWurld <162498205+WhilzWurld@users.noreply.github.com>

--- code/innobyte/group-49/Linkedlist.js
+++ code/innobyte/group-49/Linkedlist.js
@@ -38,5 +38,54 @@ class LinkedList {
             return false; // Return false if index is out of range
         }
 
+                const newNode = new Node(data); // Create a new node with the given data
+
+        if (index === 0) { // If inserting at the beginning
+            newNode.next = this.head; // Set the next pointer of the new node to the current head
+            this.head = newNode; // Set the new node as the head
+        } else {
+            let current = this.head; // Start traversal from the head
+            let prev = null; // Pointer to the previous node, initially set to null
+            let i = 0;
+
+            while (i < index) { // Traverse until reaching the desired index
+                prev = current;
+                current = current.next;
+                i++;
+            }
+
+            newNode.next = current; // Set the next pointer of the new node to the current node
+            prev.next = newNode; // Set the next pointer of the previous node to the new node
+        }
+
+        this.size++; // Increment the size of the list
+        return true; // Return true indicating successful insertion
+    }
+
+    // Method to remove the node at a specific position in the list
+    removeAt(index) {
+        if (index < 0 || index >= this.size || !this.head) { // Invalid index or empty list check
+            return null; // Return null if index is out of range or list is empty
+        }
+
+        let current = this.head; // Start traversal from the head
+        let prev = null; // Pointer to the previous node, initially set to null
+        let i = 0;
+
+        if (index === 0) { // If removing the first node
+            this.head = current.next; // Update the head to the next node
+        } else {
+            while (i < index) { // Traverse until reaching the desired index
+                prev = current;
+                current = current.next;
+                i++;
+            }
+
+            prev.next = current.next; // Set the next pointer of the previous node to the next node of the current node
+        }
+
+        this.size--; // Decrement the size of the list
+        return current.data; // Return the data of the removed node
+    }
 
 }
