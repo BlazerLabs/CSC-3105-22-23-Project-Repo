ffe4cb5519998c03a27e001cb39ded7edb52f994
Chrisnkom <162497810+Chrisnkom@users.noreply.github.com>
2024-04-05 17:40:45 +0100

    created stack class, properties with constructor
    Signed-off-by: Chrisnkom <162497810+Chrisnkom@users.noreply.github.com>

--- code/innobyte/group-54/Stack.js
+++ code/innobyte/group-54/Stack.js
@@ -1,1 +1,22 @@
+// Stack Data Structure task implementation in JavaScript
+// Author: Nkom Nkom Ibar
+// 20/CSC/081
 
+/**
+ * Stack data structure implementation.
+ */
+class StackDS
+{
+    private $stack; // Private property to hold the stack elements
+    private $top;   // Private property to keep track of the top of the stack
+
+    /**
+     * Constructor to initialize the stack.
+     */
+    public function __construct()
+    {
+        $this->stack = []; // Initialize the stack as an empty array
+        $this->top = -1;   // Initialize the top pointer to -1 (empty stack)
+    }
+
+}
