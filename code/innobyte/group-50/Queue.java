6beb64ddc59f5e800fd9211e5e6b77ffed03ad38
EMMYFALZ <162484728+EMMYFALZ@users.noreply.github.com>
2024-04-05 13:32:23 +0000

    started code, added properties & constructor methods for the class
    Signed-off-by: EMMYFALZ <162484728+EMMYFALZ@users.noreply.github.com>

--- code/innobyte/group-50/Queue.java
+++ code/innobyte/group-50/Queue.java
@@ -1,1 +1,26 @@
+// Queue data structure task implemented in java
+// Author: Okem Emmanuel Friday
+// Reg No: 20/CSC/263
+
+public class Queue<T> {
+    private static final int DEFAULT_CAPACITY = 10; // Default capacity of the queue
+    private T[] elements; // Array to store elements of the queue
+    private int size; // Current number of elements in the queue
+    private int front; // Index of the front element
+    private int rear; // Index of the rear element
+
+    // Constructor to initialize the queue with default capacity
+    public Queue() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    // Constructor to initialize the queue with a specified capacity
+    public Queue(int capacity) {
+        elements = (T[]) new Object[capacity]; // Create an array of specified capacity to store elements
+        size = 0; // Initialize the size to zero as the queue is empty initially
+        front = 0; // Initialize the front index
+        rear = -1; // Initialize the rear index to -1 as there are no elements in the queue
+    }
+
+}
 
