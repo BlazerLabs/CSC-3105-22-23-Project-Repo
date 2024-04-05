3ce72a17b54a87ed496a1359b04207e6a18695d8
WhilzWurld <162498205+WhilzWurld@users.noreply.github.com>
2024-04-05 16:06:37 +0100

    Update Linkedlist.js, added class and constructors
    Signed-off-by: WhilzWurld <162498205+WhilzWurld@users.noreply.github.com>

--- code/innobyte/group-49/Linkedlist.js
+++ code/innobyte/group-49/Linkedlist.js
@@ -1,1 +1,19 @@
+//LinkedList implementation task using JavaScript
+//Author: Ukweni Godswill Ekpezu
+//Reg No: 20/CSC/266
+// Node class represents a single element in the linked list
+class Node {
+    constructor(data) {
+        this.data = data; // Data stored in the node
+        this.next = null; // Pointer to the next node, initially set to null
+    }
+}
 
+// LinkedList class represents the linked list data structure
+class LinkedList {
+    constructor() {
+        this.head = null; // Pointer to the first node in the list, initially set to null
+        this.size = 0; // Size of the linked list, initially set to 0
+    }
+
+}
