cece5205d7e063b766e9f572e757873b8f6dd912
Emmyrexoko <162504836+Emmyrexoko@users.noreply.github.com>
2024-04-07 17:42:19 +0000

    Updated Queue.py with class, constructor and one method
    Signed-off-by: Emmyrexoko <162504836+Emmyrexoko@users.noreply.github.com>

--- code/innobyte/group-53/Queue.py
+++ code/innobyte/group-53/Queue.py
@@ -1,1 +1,12 @@
+# Queue Data Structure task implementation in Python
+# Author: Oko Emmanuel Adoshi
+# Reg No: 20/CSC/106
 
+class Queue:
+    def __init__(self):
+        """Initialize an empty queue."""
+        self.items = []
+
+    def is_empty(self):
+        """Check if the queue is empty."""
+        return len(self.items) == 0
