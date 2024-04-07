06295b7b93c5c64715cfce77a5714cfca7d0d225
isaac cletus <89643817+puyela@users.noreply.github.com>
2024-04-07 19:43:41 +0000

    added add and remove methods to set class
    Signed-off-by: isaac cletus <89643817+puyela@users.noreply.github.com>

--- code/innobyte/group-52/Set.py
+++ code/innobyte/group-52/Set.py
@@ -6,3 +6,13 @@ class Set:
     def __init__(self):
         """Initialize the set."""
         self.elements = []
+        
+    def add(self, element):
+        """Add an element to the set."""
+        if element not in self.elements:
+            self.elements.append(element)
+
+    def remove(self, element):
+        """Remove an element from the set."""
+        if element in self.elements:
+            self.elements.remove(element)
