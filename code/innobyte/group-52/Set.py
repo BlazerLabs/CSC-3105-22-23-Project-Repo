270eebab30a46db040abb1e01b8ceca85579b017
isaac cletus <89643817+puyela@users.noreply.github.com>
2024-04-07 19:44:46 +0000

    Updated Set.py , added other set functionalities
    Signed-off-by: isaac cletus <89643817+puyela@users.noreply.github.com>

--- code/innobyte/group-52/Set.py
+++ code/innobyte/group-52/Set.py
@@ -16,3 +16,15 @@ class Set:
         """Remove an element from the set."""
         if element in self.elements:
             self.elements.remove(element)
+            
+    def contains(self, element):
+        """Check if the set contains a specific element."""
+        return element in self.elements
+
+    def size(self):
+        """Get the size of the set."""
+        return len(self.elements)
+
+    def to_list(self):
+        """Get all elements in the set as a list."""
+        return self.elements
