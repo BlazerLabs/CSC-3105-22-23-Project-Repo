345db915185684322ad6858232ef8a8fc9ee75cb
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 17:11:48 -0400

    Updated Graph File

--- code/futureforge/group-31/graphs.js
+++ code/futureforge/group-31/graphs.js
@@ -2,3 +2,45 @@
 
 // A graph is a non-linear data structure consisting of vertices (nodes) and edges (connections) that link pairs of vertices. 
 // Graphs are used to represent networks and relationships between entities.
+
+class Graph {
+    constructor() {
+        this.vertices = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.vertices[vertex]) {
+            this.vertices[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2) {
+        if (!this.vertices[vertex1]) {
+            this.addVertex(vertex1);
+        }
+        if (!this.vertices[vertex2]) {
+            this.addVertex(vertex2);
+        }
+        this.vertices[vertex1].push(vertex2);
+        this.vertices[vertex2].push(vertex1); // For undirected graph
+    }
+
+    printGraph() {
+        for (let vertex in this.vertices) {
+            console.log(vertex + " -> " + this.vertices[vertex].join(", "));
+        }
+    }
+}
+
+// Example usage of Graph
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "C");
+
+graph.printGraph();
