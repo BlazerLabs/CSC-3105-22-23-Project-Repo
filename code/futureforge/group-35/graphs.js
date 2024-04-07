9a292a47d5801e58bcf5f07a7b42b307fc9f0da9
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 18:04:24 -0400

    Defined Graphs DC

---
+++ code/futureforge/group-35/graphs.js
@@ -0,0 +1,5 @@
+//CEO's Work 20/CSC/119
+
+// A graph is a non-linear data structure consisting of a collection of nodes (vertices) 
+// and edges that connect pairs of nodes. Graphs are used to represent networks and relationships between entities.
+
