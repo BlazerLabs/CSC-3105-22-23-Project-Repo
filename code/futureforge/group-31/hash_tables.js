a9e9206a318187cee0fd6932e636f29907aa14f5
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 17:08:20 -0400

    Created Hash Table Class

---
+++ code/futureforge/group-31/hash_tables.js
@@ -0,0 +1,6 @@
+
+// CEO Work 20/CSC/119
+
+// Hash Table
+// A hash table (or hash map) is a data structure that implements an associative array abstract data type, 
+// where keys are mapped to values using a hash function. Hash tables offer efficient insertion, deletion, and lookup operations.
