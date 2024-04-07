c4c97d5eb749c23687f4520ed7aca1a1d9dd8d3b
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 17:33:53 -0400

    Defined Tries for Group 33

---
+++ code/futureforge/group-33/tries.py
@@ -0,0 +1,4 @@
+#CEO's Work 20/CSC/119
+
+# A trie, also known as a prefix tree, is a tree-like data structure used for efficiently storing and retrieving a dynamic set of strings. Each node in a trie represents a single character of a string. 
+# The structure of a trie allows for quick lookups and prefix-based searches. Tries are commonly used in applications involving autocomplete, spell checkers, and prefix matching.
