a12b9547b2e7f3e0d83849380c907ef9a75a0682
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 17:13:08 -0400

    Created Tries Class

---
+++ code/futureforge/group-31/tries.js
@@ -0,0 +1,5 @@
+// CEO Work 20/CSC/119
+
+// A trie (or prefix tree) is a tree-like data structure used for efficient retrieval of 
+// keys that share a common prefix. Each node in a trie represents a single character of a key, 
+// and the path from the root to a node spells out a key.
