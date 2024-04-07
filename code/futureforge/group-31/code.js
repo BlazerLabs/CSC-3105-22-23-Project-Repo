8421ddcee02c2b560bfe5b809b6f3f7a92f2f1d2
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 16:40:18 -0400

    Added Methods

--- code/futureforge/group-31/code.js
+++ code/futureforge/group-31/code.js
@@ -82,7 +82,42 @@ class Stack {
       this.items = []; // Initialize an empty array to store stack elements
   }
 
- 
+  // Push element onto the stack
+  push(element) {
+      this.items.push(element);
+  }
+
+  // Pop element from the top of the stack and return it
+  pop() {
+      if (this.isEmpty()) {
+          return "Underflow - Stack is empty";
+      }
+      return this.items.pop();
+  }
+
+  // Return the top element of the stack without removing it
+  peek() {
+      return this.items[this.items.length - 1];
+  }
+
+  // Check if the stack is empty
+  isEmpty() {
+      return this.items.length === 0;
+  }
+
+  // Return the size of the stack
+  size() {
+      return this.items.length;
+  }
+
+  // Print all elements of the stack
+  printStack() {
+      let result = "";
+      for (let i = 0; i < this.items.length; i++) {
+          result += this.items[i] + " ";
+      }
+      console.log("Stack: " + result);
+  }
 }
 
 
