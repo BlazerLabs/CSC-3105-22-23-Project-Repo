16e469bbdce993dc150fef680b9570f5abc913f8
Emediong Sunday <157766159+EmyEverBlessed@users.noreply.github.com>
2024-04-07 15:05:12 +0000

    added Stack class with initial constructor
    Signed-off-by: Emediong Sunday  <157766159+EmyEverBlessed@users.noreply.github.com>

--- code/innobyte/group-52/Stack.py
+++ code/innobyte/group-52/Stack.py
@@ -1,1 +1,9 @@
+# Stack Data Structure task implementation in Python Programming language
+# Author : Emediong Sunday
+# Reg No: 20/CSC/033
 
+#Stack class
+class Stack:
+    def __init__(self):
+        """Initialize an empty stack."""
+        self.items = []
