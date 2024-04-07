aaf44600518ff2b85244a179dbe78fa2f5048c0f
isaac cletus <89643817+puyela@users.noreply.github.com>
2024-04-07 19:40:29 +0000

    Update Set.py
    Signed-off-by: isaac cletus <89643817+puyela@users.noreply.github.com>

--- code/innobyte/group-52/Set.py
+++ code/innobyte/group-52/Set.py
@@ -1,1 +1,8 @@
+# Set Data Strucuture tasks assigned to me - group 52
+# Author: Isaac Cletus Unimnake
+# RegNo: 20/CSC/030
 
+class Set:
+    def __init__(self):
+        """Initialize the set."""
+        self.elements = []
