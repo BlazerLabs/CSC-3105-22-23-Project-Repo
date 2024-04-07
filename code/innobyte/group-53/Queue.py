6b1618e660c074d73189146b22dfdf7e4dfb17dc
Emmyrexoko <162504836+Emmyrexoko@users.noreply.github.com>
2024-04-07 17:44:23 +0000

    Updated Queue.py, added methods: enqueue and dequeue
    Signed-off-by: Emmyrexoko <162504836+Emmyrexoko@users.noreply.github.com>

--- code/innobyte/group-53/Queue.py
+++ code/innobyte/group-53/Queue.py
@@ -10,3 +10,14 @@ class Queue:
     def is_empty(self):
         """Check if the queue is empty."""
         return len(self.items) == 0
+
+    def enqueue(self, item):
+        """Add an item to the rear of the queue."""
+        self.items.append(item)
+
+    def dequeue(self):
+        """Remove and return the item at the front of the queue."""
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("dequeue from an empty queue")
