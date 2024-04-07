56e659dc7ac78cbd106b281b894d33d16c1053b8
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 17:48:04 -0400

    Created File for Graph DS

---
+++ code/futureforge/group-32/graphs.js
@@ -0,0 +1,5 @@
+// CEO's Work
+
+// A graph is a non-linear data structure consisting of a collection of nodes (vertices) 
+// and edges that connect pairs of nodes. Graphs are used to represent networks and relationships between entities.
+
