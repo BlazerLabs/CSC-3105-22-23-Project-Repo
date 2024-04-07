8a1ea80a1b56b916f763627e1a5f0e75e5399b4b
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 17:03:19 -0400

    Defined Graph

---
+++ code/futureforge/group-31/graphs.js
@@ -0,0 +1,5 @@
+// CEO Work 20/CSC/119
+
+// A tree is a hierarchical data structure consisting of nodes connected by edges. 
+// Each node in a tree has a parent node and zero or more child nodes. Trees are used to represent hierarchical relationships and are commonly used in 
+// applications like file systems, organizational charts, and parsing expressions.
