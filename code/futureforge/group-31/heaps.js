1cf677f0a3d2b7faa0a67739145bb87023a51103
Emmzy-Tech <emmzytech@gmail.com>
2024-04-07 17:05:22 -0400

    Created Heaps File

---
+++ code/futureforge/group-31/heaps.js
@@ -0,0 +1,6 @@
+// CEO Work 20/CSC/119
+
+// A heap is a specialized tree-based data structure that satisfies the heap property. In a heap, for a given node i, the value of its parent node is greater (or smaller) 
+// than or equal to the value of i depending on whether it's a max heap or min heap. Heaps are commonly used in priority queues and heap sort algorithms.
+
+
