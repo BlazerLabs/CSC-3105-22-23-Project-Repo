096a5e0e769a08cc0091228ad23ee782d5a371fb
Emediong Sunday <157766159+EmyEverBlessed@users.noreply.github.com>
2024-04-07 15:09:44 +0000

    resolved indentation bug, also added peek and size bug
    Signed-off-by: Emediong Sunday  <157766159+EmyEverBlessed@users.noreply.github.com>

--- code/innobyte/group-52/Stack.py
+++ code/innobyte/group-52/Stack.py
@@ -8,17 +8,28 @@ class Stack:
         """Initialize an empty stack."""
         self.items = []
         
-def is_empty(self):
-        """Check if the stack is empty."""
-        return len(self.items) == 0
-
+    def is_empty(self):
+            """Check if the stack is empty."""
+            return len(self.items) == 0
+    
     def push(self, item):
         """Push an item onto the top of the stack."""
         self.items.append(item)
-
+    
     def pop(self):
         """Remove and return the item at the top of the stack."""
         if not self.is_empty():
             return self.items.pop()
         else:
             raise IndexError("pop from an empty stack")
+            
+    def peek(self):
+        """Return the item at the top of the stack without removing it."""
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("peek from an empty stack")
+
+    def size(self):
+        """Return the number of items in the stack."""
+        return len(self.items)
