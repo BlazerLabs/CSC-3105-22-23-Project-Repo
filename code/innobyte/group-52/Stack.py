430f0fc1413fffa759d83bc54c74bcf6f04db7b4
Emediong Sunday <157766159+EmyEverBlessed@users.noreply.github.com>
2024-04-07 15:07:05 +0000

    Update Stack.py, added push and pop methods
    Signed-off-by: Emediong Sunday  <157766159+EmyEverBlessed@users.noreply.github.com>

--- code/innobyte/group-52/Stack.py
+++ code/innobyte/group-52/Stack.py
@@ -7,3 +7,18 @@ class Stack:
     def __init__(self):
         """Initialize an empty stack."""
         self.items = []
+        
+def is_empty(self):
+        """Check if the stack is empty."""
+        return len(self.items) == 0
+
+    def push(self, item):
+        """Push an item onto the top of the stack."""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove and return the item at the top of the stack."""
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("pop from an empty stack")
