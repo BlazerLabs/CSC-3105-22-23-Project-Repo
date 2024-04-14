d9bdd76c4e7ef31e8abb911bbcf95aee9486723e
maRia <163128726+doMisaac@users.noreply.github.com>
2024-04-14 08:08:23 +0100

    Create Group 18.php
    Signed-off-by: maRia <163128726+doMisaac@users.noreply.github.com>

---
+++ code/codecraft/Project 2/Group 18.php
@@ -0,0 +1,24 @@
+<?php
+
+// Define the Person class
+class Person {
+    // Properties
+    public $name;
+    public $age;
+}
+
+// Instantiate an object of the Person class
+$person = new Person();
+
+// Set values for the object's properties
+$person->name = "Alice";
+$person->age = 30;
+
+// Output the object's properties
+echo "Name: " . $person->name . "<br>";
+echo "Age: " . $person->age;
+
+
+// Group 18
+// 20/CSC/065
+?>
